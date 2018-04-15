92f352d78e88695999c6984bf6bd136b2dee572a
busta <chris011972@gmail>
2018-04-15 17:17:19 -0400

    @ hotel

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -42,35 +42,35 @@ class HomeController extends Controller
 
 	    $url = sprintf('https://www.instagram.com/%s/?__a=1', $social);
 
-	   // $response = $client->get($url);
+	    $response = $client->get($url);
 
-        //$items = array($response->getBody()->getContents());
-	    //$items = json_decode((string) $response->getBody()->getContents(), true)['items'];
+        $items = array($response->getBody()->getContents());
+	    $items = json_decode((string) $response->getBody()->getContents(), true)['items'];
 
-      //  var_dump($items);
-      //  die();
+        var_dump($items);
+        die();
 
 
         /////// CONFIG ///////
-        $username = 'infatuationapparel';
-        $password = '1219Whittier';
-        $debug = false;
-        $truncatedDebug = false;
+       // $username = 'infatuationapparel';
+        //$password = '1219Whittier';
+       // $debug = false;
+       // $truncatedDebug = false;
         //////////////////////
            
-        $ig = new \InstagramAPI\Instagram($debug, $truncatedDebug);
+       // $ig = new \InstagramAPI\Instagram($debug, $truncatedDebug);
 
 
-        try {
-            $ig->login($username, $password);
-        } catch (\Exception $e) {
-            echo 'Something went wrong: '.$e->getMessage()."\n";
-            exit(0);
-        }
+       // try {
+       //     $ig->login($username, $password);
+       // } catch (\Exception $e) {
+       //     echo 'Something went wrong: '.$e->getMessage()."\n";
+        //    exit(0);
+       // }
 
-        $userId = $ig->people->getUserIdForName($social);
+      //  $userId = $ig->people->getUserIdForName($social);
 
-        $items = $ig->people->getInfoById($userId);
+      //  $items = $ig->people->getInfoById($userId);
 
 
 
